48776f16ab276c5077866af9f75e5b24a5809246
Jesse Songok <jessesongok@gmail.com>
2022-05-23 11:40:09 +0300

    Products Migration

--- database/migrations/2022_05_23_083406_create_products_table.php
+++ database/migrations/2022_05_23_083406_create_products_table.php
@@ -11,7 +11,7 @@ class CreateProductsTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up()   
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
